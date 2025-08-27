66f2fe73616f7b8743ade6d672bf86a0c5d2e65e
a-jain24 <dhvanijain@gmail.com>
2025-08-27 15:17:48 -0500

    SUIT atlas runs msc01

--- code/functional_connectivity/midnight_scan_club/msc_extract_timeseries.py
+++ code/functional_connectivity/midnight_scan_club/msc_extract_timeseries.py
@@ -15,14 +15,16 @@ def fetch_atlas(atlas_name, atlas_dir=None):
     if atlas_name == 'HarvardOxford':
         atlas = datasets.fetch_atlas_harvard_oxford('sub-maxprob-thr25-1mm', data_dir=atlas_dir)
     elif atlas_name == "Schaefer":
-        atlas = datasets.fetch_atlas_schaefer_2018(n_rois=100, yeo_networks=7, resolution_mm=1, data_dir=None, base_url=None, resume=True, verbose=1)
+        atlas = datasets.fetch_atlas_schaefer_2018(n_rois=100, yeo_networks=17, resolution_mm=2, data_dir=None, base_url=None, resume=True, verbose=1)
     elif atlas_name == 'MSDL':
         atlas = datasets.fetch_atlas_msdl()
-    elif atlas_name == 'Cerebellum':
-        # Assuming the cerebellum atlas is already downloaded and available locally
-        atlas = nib.load(os.path.join(atlas_dir, 'Cerebellum-MNIsegment-1segment.nii'))
+    elif atlas_name == 'SUIT':
+        atlas = {}
+        atlas['img_path'] = '/mfs/io/groups/dmello/projects/egcerebellum/code/connectivity/correlation/parcellations/atl-Anatom_space-MNI_dseg_resamplespace-MNI152NLin2009cAsym_res-2.nii'
+        atlas['lut_path'] = '/mfs/io/groups/dmello/projects/egcerebellum/code/connectivity/correlation/parcellations/atl-Anatom.tsv'
+
     else:
-        raise ValueError("Atlas not recognized. Please choose 'HarvardOxford', 'MSDL', or 'Cerebellum'.")
+        raise ValueError("Atlas not recognized.")
     return atlas
 
 def get_masker(atlas, atlas_name):
@@ -53,6 +55,18 @@ def get_masker(atlas, atlas_name):
     elif atlas_name == 'Schaefer':
         masker = MultiNiftiLabelsMasker(
         labels_img=atlas.maps,  # Both hemispheres
+        resampling_target="data",
+        standardize="zscore_sample",
+        standardize_confounds="zscore_sample",
+        memory="nilearn_cache",
+        n_jobs=2,
+    ).fit()
+
+    elif atlas_name == 'SUIT':
+        masker = MultiNiftiLabelsMasker(
+        labels_img = atlas['img_path'],  # Both hemispheres
+        lut = atlas['lut_path'],
+        resampling_target="data",
         standardize="zscore_sample",
         standardize_confounds="zscore_sample",
         memory="nilearn_cache",
@@ -74,9 +88,7 @@ def get_pooled(base_url, file_ids, masker, subject_id='MSC01', session='func01')
     pooled_subject = []
 
     for file_id in file_ids:
-        # skip empty file_ids
-        if file_id == '':
-            continue
+
         # construct the URL for the subject's fMRI data
         time_series = masker.transform(construct_url(base_url, file_id, subject_id=subject_id, session=session))
         pooled_subject.append(time_series)
@@ -105,6 +117,26 @@ def save_data(pooled_subject, atlas_name, file_ids, output_dir='output/roi_time_
         np.savetxt(f'{this_output_dir}/pooled/{file_id}.csv', task, delimiter=',')
         np.savetxt(f'{this_output_dir}/shape/{file_id}.csv', shape, delimiter=',')
 
+# filter file ids based on subject and session
+def filter_file_ids(file_ids, base_url, subject_id='MSC01', session='func01'):
+    
+    new_file_ids = []
+    for file_id in file_ids:
+        
+        if file_id == '':
+            continue
+            
+        url = construct_url(base_url, file_id, subject_id=subject_id, session=session)
+        
+        # check if the file exists at the path given by url
+        if not os.path.exists(url):
+            print(f"File {file_id} not found at {url}, skipping...")
+        
+        else:
+            new_file_ids.append(file_id)
+    
+    return new_file_ids
+
 
 # extract cortical time series using the MSDL atlas
 def extract_time_series(base_url, file_ids, atlas_name='MSDL', tasks="all_tasks", subject_id='MSC01', session='func01'):
@@ -114,6 +146,8 @@ def extract_time_series(base_url, file_ids, atlas_name='MSDL', tasks="all_tasks"
 
     # create masker based on the atlas type
     masker = get_masker(atlas, atlas_name)  
+    
+    file_ids = filter_file_ids(file_ids, base_url, subject_id=subject_id, session=session)
 
     # get pooled subjects time series
     pooled_subject = get_pooled(base_url, file_ids, masker, subject_id=subject_id, session=session)
@@ -129,15 +163,15 @@ def main():
     
     
     try_rest = False # just try running the rest condition
+    run_all_sessions = True # run all sessions if True or just try one
     tasks = "all_tasks"
-    atlas_name = 'Schaefer'
+    atlas_name = 'SUIT'
 
-    # set the subject and session
-    subject_id = "MSC01"
-    session = "func01"
+    subject_ids = ["MSC01"]  # change this to the subjects you want to run
+    session = "func01" # set if only running one session
 
     # CHANGE PATHS HERE
-    base_url = f"/mfs/io/groups/dmello/projects/cerebellum_reliability/derivatives/fmriprep/ds000224/sub-{subject_id}/ses-{session}/func"
+    # base_url = f"/mfs/io/groups/dmello/projects/cerebellum_reliability/derivatives/fmriprep/ds000224/sub-{subject_id}/ses-{session}/func"
 
     ids_path = "/mfs/io/groups/dmello/projects/dynamric/fmri_connectivity_trees/datasets/midnight_scan_club/msc_file_ids.txt"
     with open(ids_path, 'r') as f:
@@ -151,14 +185,23 @@ def main():
             file_ids = [x.strip() for x in file_ids]
         tasks = "rest"
     
-    run_all_sessions = True
-    if run_all_sessions:
-        all_sessions = ['func01', 'func02', 'func03', 'func04', 'func05', 'func06', 'func07', 'func08', 'func09', 'func10']
-        for session in all_sessions[1:]:
+    
+    for subject_id in subject_ids:
+        if run_all_sessions:
+            all_sessions = [
+                            # 'func01', 
+                            'func02', 
+                            'func03', 'func04', 'func05', 
+                            'func06', 
+                            'func07', 
+                            'func08', 'func09', 'func10'
+                            ]
+            for session in all_sessions:
+                base_url = f"/mfs/io/groups/dmello/projects/cerebellum_reliability/derivatives/fmriprep/ds000224/sub-{subject_id}/ses-{session}/func"
+                extract_time_series(base_url, file_ids, subject_id=subject_id, atlas_name=atlas_name, session=session, tasks=tasks)
+        else:
             base_url = f"/mfs/io/groups/dmello/projects/cerebellum_reliability/derivatives/fmriprep/ds000224/sub-{subject_id}/ses-{session}/func"
             extract_time_series(base_url, file_ids, subject_id=subject_id, atlas_name=atlas_name, session=session, tasks=tasks)
-    else:
-        extract_time_series(base_url, file_ids, subject_id=subject_id, atlas_name=atlas_name, session=session, tasks=tasks)
 
 
 if __name__ == "__main__":
