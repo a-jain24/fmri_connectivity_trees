27e1b6b56aad43619b9e97852667feb17a9261fe
a-jain24 <dhvanijain@gmail.com>
2025-10-06 12:06:44 -0500

    up to date mi

--- code/functional_connectivity/midnight_scan_club/msc_mutual_info.py
+++ code/functional_connectivity/midnight_scan_club/msc_mutual_info.py
@@ -45,7 +45,7 @@ def combine_cort_and_cereb(cort_concat_timeseries, cereb_concat_timeseries, use_
 def combine_timeseries(subjects, timeseries_array, use_torch=True):
 
     if len(timeseries_array) < 2:
-        return timeseries_array[0]
+        return get_concat(timeseries_array[0], use_torch=use_torch)
     
     concat_timeseries = {}
     for subject in subjects:
@@ -199,6 +199,8 @@ def get_mi_matrices(atlases, subjects, tasks, sessions, base_dir):
                 shape_path = f'{base_dir}/code/functional_connectivity/midnight_scan_club/output/roi_time_series/{subject}/{session}/{atlas}/all_tasks/shape/'
                 pooled_path = f'{base_dir}/code/functional_connectivity/midnight_scan_club/output/roi_time_series/{subject}/{session}/{atlas}/all_tasks/pooled/'
                 timeseries[subject] = timeseries[subject] + get_timeseries(tasks, shape_path, pooled_path)
+        
+        timeseries_array.append(get_concat(timeseries))
 
     concat_timeseries = combine_timeseries(subjects, timeseries_array)
 
